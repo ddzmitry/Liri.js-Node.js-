b6e6a986cf3729a58e5bd578a6b0cec913788e6d
Dzmitry Dubarau <ddzmitry@yahoo.com>
2017-02-08 00:52:08 -0500

    Liri

---
+++ key.js
@@ -0,0 +1,6 @@
+exports.twitterKeys = {
+  consumer_key: 'k6y3Ebf5VNYDmoOwxnhvOEKzp',
+  consumer_secret: '5yHNedLVDj5mLJ28z6qYRggWGtC40qVpIQFavyjp6ukOoBd6Uq',
+  access_token_key: '4071521775-5MPQSqUD4PxPEMWIFA6LgylZgGr2bZFZBbbjURe',
+  access_token_secret: 'dIx5NE548ilShopwCH4RCzJpWRPf0RJYXqja7J1y9PWX4',
+}
